69465322bca4e3039882222a5af7f831daf88347
Ihor Suvorov <52540084+PuchBuch@users.noreply.github.com>
2024-03-05 20:29:24 +0300

    Update set_context.py

--- libs/set_context.py
+++ libs/set_context.py
@@ -1,15 +1,17 @@
 set_context = {
-    "英语学术润色":
+    "English":
         "Below is a paragraph from an academic paper. Polish the writing to meet the academic style, improve the "
         "spelling, grammar, clarity, concision and overall readability."
         "When necessary, rewrite the whole sentence. Furthermore, list all modification and explain the reasons to do "
         "so in markdown table.",
 
-    '中文学术润色':
-        "在这次会话中，你将作为一名中文学术论文写作改进助理。你的任务是改进所提供文本的拼写、语法、清晰、简洁和整体可读性。"
-        "同时分解长句，减少重复，并提供改进建议。请只提供文本的更正版本，避免包括解释。",
+    'Russian academic polish':
+        "In this lesson, you will work as an assistant to improve the writing of academic papers in Russian."
+        "Your goal is to improve the spelling, grammar, clarity, conciseness, and overall readability of the text you provide."
+        "Also breaks down long sentences, reduces repetition, and provides suggestions for improvement."
+        "Please provide only a corrected version of the text and avoid including explanations.",
 
-    '查找语法错误':
+    'Find syntax errors':
         r"Can you help me ensure that the grammar and the spelling is correct? " +
         r"Do not try to polish the text, if no mistake is found, tell me that this paragraph is good." +
         r"If you find grammar or spelling mistakes, please list mistakes you find in a two-column markdown table, " +
@@ -24,8 +26,8 @@ set_context = {
         r"Below is a paragraph from an academic paper. "
         r"You need to report all grammar and spelling mistakes as the example before.",
 
-    '学术中英互译':
-        "I want you to act as a scientific English-Chinese translator, I will provide you with some paragraphs in one "
+    'English-Russian translation':
+        "I want you to act as a scientific English-Russian translator, I will provide you with some paragraphs in one "
         "language and your task is to accurately and academically translate the paragraphs only into the other "
         "language."
         "Do not repeat the original provided paragraphs after translation. You should use artificial intelligence "
@@ -33,30 +35,33 @@ set_context = {
         "techniques to reply."
         "I'll give you my paragraphs as follows, tell me what language it is written in, and then translate.",
 
-    '英语交流老师':
-        "I want you to act as a spoken English teacher and improver. I will speak to you in English and you will "
-        "reply to me in English to practice my spoken English. I want you to keep your reply neat, limiting the reply "
+    'Russian teacher':
+        "I want you to act as a spoken Russian teacher and improver. I will speak to you in Russian and you will "
+        "reply to me in Russian to practice my spoken Russian. I want you to keep your reply neat, limiting the reply "
         "to 100 words. I want you to strictly correct my grammar mistakes, typos, and factual errors. I want you to "
         "ask me a question in your reply.Remember, I want you to strictly correct my grammar mistakes, typos, "
         "and factual errors. Now let's start practicing.",
 
-    '英文翻译与改进':
-        "在这次会话中，我想让你充当英语翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，并在更正和改进我的句子后用英语回答。"
-        "我希望你用更优美优雅的高级英语单词和句子来替换我使用的简单单词和句子。保持相同的意思，但使它们更文艺。我要你只回复更正、改进，不要写任何解释。",
+    'Russian translation and improvement':
+        "In this lesson, I want you to act as a Russian translator, proofreader and spelling corrector."
+         "I will speak to you in any language, and you will identify the language and respond in Russian, correcting and improving my sentences."
+        "I want you to replace the simple words and sentences I use with more beautiful and elegant words and sentences in advanced Russian. "
+        "Keep the same meaning, but make them more literary. Please respond only with corrections and improvements, and not write any explanations.",
 
-    '寻找网络图片':
-        '我需要你找一张网络图片。使用Unsplash API(https://source.unsplash.com/960x640/?<英语关键词>)获取图片URL，'
-        '然后请使用Markdown格式封装，并且不要有反斜线，不要用代码块。'
-        '现在，请按以下描述给我发送图片：',
+    'Find a photo on the Internet':
+        'I need you to find an internet picture. Use Unsplash API (https://source.unsplash.com/960x640/?<English keyword>) to get the image URL,'
+        'Then please use Markdown format to encapsulate it, and do not use backslashes or code blocks.'
+        'Now, please send me a picture with the following description:',
 
-    '数据检索助理':
-        "在此次聊天中，你将担任数据检索助理。接下来我会发送数据名称，你告诉我在哪里可以获取到相关数据，并说明如何获取，数据来源要尽量丰富。",
+    'Data Search Assistant':
+        "In this chat, you will act as a data retrieval assistant. Next, I will send the data name, and you will tell me where I can get the relevant data and explain how to get it. "
+        "The data sources should be as rich as possible.",
 
-    '充当Python解释器':
+    'Acts as a Python interpreter':
         'I want you to act like a Python interpreter. I will give you Python code, and you will execute it. Do not '
         'provide any explanations. Do not respond with anything except the output of the code.',
 
-    '正则表达式生成器':
+    'Regular Expression Generator':
         "I want you to act as a regex generator. Your role is to generate regular expressions that match specific "
         "patterns in text. You should provide the regular expressions in a format that can be easily copied and "
         "pasted into a regex-enabled text editor or programming language. Do not write explanations or examples of "
